b08995471cc8f80f73ef9f5a11050acc627c205e
Harpreet <harpreet13108159@gmail.com>
2026-09-09 22:39:37 +0530

    dockerize backend and add mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 import 'dotenv/config';
 import app from './app.js';
-
+import connectDB from './config/db.js';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Backend is running on port ${PORT}`);
-});
+
+async function startServer() {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Backend is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        process.exit(1);
+    }
+}
+
+startServer();
