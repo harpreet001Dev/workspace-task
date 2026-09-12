c6eaa5a6601a13f3cc06d6757c1c646ddc3e22f5
Harpreet <harpreet13108159@gmail.com>
2026-09-12 16:36:33 +0530

    testing ci flow

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,10 +1,13 @@
 import Redis from 'ioredis';
 
+//redis connection
+
 const redisConnection = new Redis({
   host: 'redis',
   port: 6379,
   maxRetriesPerRequest: null,
 });
+
 
 redisConnection.on('connect', () => {
   console.log('Redis connected');
